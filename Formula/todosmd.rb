a4b0e0d0e1b25d91e77eed6f08ef5f0c571c30a6
Luan Nguyen <lihavim@gmail.com>
2026-01-09 17:33:32 +0700

    Update todosmd to 0.1.2

--- Formula/todosmd.rb
+++ Formula/todosmd.rb
@@ -1,8 +1,8 @@
 class Todosmd < Formula
   desc "Terminal-based task manager for Markdown files with vim keybindings"
   homepage "https://github.com/peeomid/todos.md"
-  url "https://registry.npmjs.org/todosmd/-/todosmd-0.1.1.tgz"
-  sha256 "479a5d502582fe1931c046b179cbb25742738343fe95414a705b58f83717108b"
+  url "https://registry.npmjs.org/todosmd/-/todosmd-0.1.2.tgz"
+  sha256 "84f8cd83ce1080ca8bba00e84bb28671d7a09eb9961b468a7062124c6c24ce11"
   license "MIT"
 
   depends_on "node@20"
