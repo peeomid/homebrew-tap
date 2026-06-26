4f6007ba76619286fb3d6f581a8d60d28a3125a9
Luan Nguyen <lihavim@gmail.com>
2026-06-26 17:16:15 +0700

    Add readmd formula

---
+++ Formula/readmd.rb
@@ -0,0 +1,18 @@
+class Readmd < Formula
+  desc "Render Markdown into beautiful standalone HTML reading pages"
+  homepage "https://github.com/peeomid/readmd"
+  url "https://github.com/peeomid/readmd.git", branch: "master"
+  version "0.1.0"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    (testpath/"note.md").write "# Hello\n"
+    assert_match "<!doctype html>", shell_output("#{bin}/readmd #{testpath}/note.md --stdout")
+  end
+end
